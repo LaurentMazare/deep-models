404535fe83579b78a994bde3dd33a13f8dc31781
laurent <laurent.mazare@gmail.com>
2016-11-07 23:04:35 +0000

    Only gpu mode for now.

--- rhn/lstm_ln.py
+++ rhn/lstm_ln.py
@@ -16,9 +16,10 @@ class LSTM_LN(LSTM):
   def build(self, input_shape):
     super(LSTM_LN, self).build(input_shape)
     self.gs, self.bs = [], []
-    for i in xrange(5):
-      self.gs += [ self.init((self.output_dim,), name='{}_g%i'.format(self.name, i)) ]
-      self.bs += [ K.zeros((self.output_dim,), name='{}_b%d'.format(self.name, i)) ]
+    for i in xrange(3):
+      f = 1 if i == 2 else 4
+      self.gs += [ self.init((f*self.output_dim,), name='{}_g%i'.format(self.name, i)) ]
+      self.bs += [ K.zeros((f*self.output_dim,), name='{}_b%d'.format(self.name, i)) ]
     self.trainable_weights += self.gs + self.bs
 
   def step(self, x, states):
@@ -28,18 +29,19 @@ class LSTM_LN(LSTM):
     B_W = states[3]
 
     if self.consume_less == 'gpu':
-      z = K.dot(x * B_W[0], self.W) + K.dot(h_tm1 * B_U[0], self.U) + self.b
+      z = self.norm(K.dot(x * B_W[0], self.W), 0) + self.norm(K.dot(h_tm1 * B_U[0], self.U), 1) + self.b
 
       z0 = z[:, :self.output_dim]
       z1 = z[:, self.output_dim: 2 * self.output_dim]
       z2 = z[:, 2 * self.output_dim: 3 * self.output_dim]
       z3 = z[:, 3 * self.output_dim:]
 
-      i = self.inner_activation(self.norm(z0, 0))
-      f = self.inner_activation(self.norm(z1, 1))
-      c = f * c_tm1 + i * self.activation(self.norm(z2, 2))
-      o = self.inner_activation(self.norm(z3, 3))
+      i = self.inner_activation(z0)
+      f = self.inner_activation(z1)
+      c = f * c_tm1 + i * self.activation(z2)
+      o = self.inner_activation(z3)
     else:
+      assert (False)
       if self.consume_less == 'cpu':
         x_i = x[:, :self.output_dim]
         x_f = x[:, self.output_dim: 2 * self.output_dim]
@@ -53,10 +55,10 @@ class LSTM_LN(LSTM):
       else:
         raise Exception('Unknown `consume_less` mode.')
 
-      i = self.inner_activation(self.norm(x_i + K.dot(h_tm1 * B_U[0], self.U_i), 0))
-      f = self.inner_activation(self.norm(x_f + K.dot(h_tm1 * B_U[1], self.U_f), 1))
-      c = f * c_tm1 + i * self.activation(self.norm(x_c + K.dot(h_tm1 * B_U[2], self.U_c), 2))
-      o = self.inner_activation(self.norm(x_o + K.dot(h_tm1 * B_U[3], self.U_o), 3))
+      i = self.inner_activation(x_i + K.dot(h_tm1 * B_U[0], self.U_i))
+      f = self.inner_activation(x_f + K.dot(h_tm1 * B_U[1], self.U_f))
+      c = f * c_tm1 + i * self.activation(x_c + K.dot(h_tm1 * B_U[2], self.U_c))
+      o = self.inner_activation(x_o + K.dot(h_tm1 * B_U[3], self.U_o))
 
-    h = o * self.activation(self.norm(c, 4))
+    h = o * self.activation(self.norm(c, 2))
     return h, [h, c]
