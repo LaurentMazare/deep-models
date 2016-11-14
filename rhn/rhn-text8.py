90a642f4bf6044159155616a5805fba1ae2bb47d
laurent <laurent.mazare@gmail.com>
2016-11-14 21:41:14 +0000

    Cleanup.

--- rhn/rhn-text8.py
+++ rhn/rhn-text8.py
@@ -10,10 +10,12 @@ from keras.layers import Embedding, Dense, LSTM, TimeDistributed
 from keras.models import Sequential
 from rhn import RHN
 from lstm_ln import LSTM_LN
+from lstm_zoneout import LSTM_zoneout
 
 seq_len = 180
-batch_size = 128
-epochs = 10
+train_batch_size = 128
+test_batch_size = 4096
+epochs = 1000
 rhn_size = 256
 
 def load(filename):
@@ -36,6 +38,7 @@ def batchXY(start_idx, length, slen=seq_len):
     Y.append(Xs[idx+1:idx+slen+1])
   return np.array(X), np.array(Y)
 
+model_name = 'lstm'
 train_lbatch = 18
 lbatch_size = 5*10**6
 validX, validY = batchXY(train_lbatch*lbatch_size, lbatch_size, slen=4096)
@@ -43,10 +46,15 @@ print "Valid", np.shape(validX), np.shape(validY)
 
 model = Sequential()
 input_shape=(None, dim)
-if False:
-  model.add(RHN(rhn_size, 2, return_sequences=True, dropout_W=0.5, dropout_U=0.5, consume_less='cpu', input_shape=input_shape))
-else:
+if model_name == 'rhn':
+  model.add(RHN(rhn_size, 2, return_sequences=True, consume_less='cpu', input_shape=input_shape))
+elif model_name == 'lstm-zoneout':
+  model.add(LSTM_zoneout(rhn_size, zoneout_c=0.5, zoneout_h=0.05,
+    return_sequences=True, consume_less='gpu', input_shape=input_shape))
+elif model_name == 'lstm':
   model.add(LSTM(rhn_size, return_sequences=True, consume_less='gpu', input_shape=input_shape))
+else:
+  raise(Exception('Unknown model %s' % model_name))
 
 model.add(TimeDistributed(Dense(dim, activation='softmax'), input_shape=(None, rhn_size)))
 optimizer = keras.optimizers.Adam(lr=0.001, clipnorm=1.0)
@@ -56,14 +64,16 @@ model.compile(loss='categorical_crossentropy',
               optimizer=optimizer,
               metrics=['accuracy'])
 
-losses = []
+bpcs = []
 for epoch_idx in xrange(epochs):
   start_time = time.time()
   for cnt in xrange(train_lbatch):
     X, Y = batchXY(cnt * lbatch_size, lbatch_size)
-    model.fit(X, Y, batch_size=batch_size, nb_epoch=1)
-  loss = model.evaluate(validX, validY, batch_size=batch_size)
-  loss[0] /= np.log(2)
-  losses.append(loss)
+    model.fit(X, Y, batch_size=train_batch_size, nb_epoch=1)
+  # Recompute the loss on the last batch
+  train_loss = model.evaluate(X, Y, batch_size=test_batch_size)
+  loss = model.evaluate(validX, validY, batch_size=test_batch_size)
+  bpcs.append((train_loss[0]/np.log(2), loss[0]/np.log(2)))
   print epoch_idx, time.time() - start_time
-  print losses
+  print bpcs
+  model.save('iter%d.h5' % epoch_idx)
